353d224e7bbb904eb78a3b6eeddc3c8efcc86261
Ivan Khomutov <i.khomutov@protonmail.com>
2019-04-16 10:55:19 +0300

    Added whitenoise in wsgi.py

--- shmango/wsgi.py
+++ shmango/wsgi.py
@@ -1,9 +1,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "shmango.settings.prod"
-)
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
