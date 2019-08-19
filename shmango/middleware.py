77d9043afb55ee707ad5d44123eccbd6d5fe1363
Ivan Khomutov <i.khomutov@protonmail.com>
2019-08-19 12:35:59 +0300

    Updated middleware syntax

--- shmango/middleware.py
+++ shmango/middleware.py
@@ -1,10 +1,12 @@
-from django.utils.deprecation import MiddlewareMixin
+class ExtractIPMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-
-class ExtractIPMiddleware(MiddlewareMixin):
-    def process_request(self, request):
+    def __call__(self, request):
         ip = request.META.get("HTTP_X_FORWARDED_FOR")
         if ip:
             request.ip = ip.split(", ")[0]
         else:
             request.ip = request.META.get("REMOTE_ADDR")
+        
+        return self.get_response(request)
